da8461e6462a7b1a841a2abf6dd11d0dc95f1b6d
Dzianis Kachanau <Dzianis.Kachanau@standardbank.co.za>
2023-05-23 09:20:19 +0300

    Part 4b: Loading Metadata Upfront

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,2 +1,14 @@
+import { loadRemoteEntry } from '@angular-architects/module-federation';
 import('./bootstrap')
-	.catch(err => console.error(err));
+  .catch(err => console.error(err));
+
+Promise.all([
+  loadRemoteEntry({
+    type: 'module',
+    remoteEntry: 'http://localhost:4201/remoteEntry.js',
+  }),
+])
+  .catch((err) => console.error('Error loading remote entries', err))
+  .then(() => import('./bootstrap'))
+  .catch((err) => console.error(err));
+
