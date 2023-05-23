67720dc6810c4ea134e3d6c50ca181514cbba57c
Dzianis Kachanau <Dzianis.Kachanau@standardbank.co.za>
2023-05-23 10:22:19 +0300

    Part 6: Explicitly Configure Shared Dependencies
    https://github.com/angular-architects/module-federation-plugin/blob/main/libs/mf/tutorial/tutorial.md#bonus---part-6-explicitly-configure-shared-dependencies

--- projects/mfe1/webpack.config.js
+++ projects/mfe1/webpack.config.js
@@ -1,4 +1,8 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  share,
+  // shareAll,
+  withModuleFederationPlugin
+} = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
@@ -8,8 +12,34 @@ module.exports = withModuleFederationPlugin({
     './Module': './projects/mfe1/src/app/flights/flights.module.ts',
   },
 
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  // shared: {
+  //   ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  // },
 
+  // Explicitly share packages:
+  shared: share({
+    '@angular/core': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common/http': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+  }),
+
+  // Explicitly share mono-repo libs:
+  sharedMappings: ['auth-lib'],
 });
--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,4 +1,8 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  share,
+  /* shareAll, */
+  withModuleFederationPlugin
+} = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
@@ -6,8 +10,34 @@ module.exports = withModuleFederationPlugin({
   //   "mfe1": "http://localhost:4201/remoteEntry.js",
   // },
 
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  // shared: {
+  //   ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  // },
 
+  // Explicitly share packages:
+  shared: share({
+    '@angular/core': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common/http': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+  }),
+
+  // Explicitly share mono-repo libs:
+  sharedMappings: ['auth-lib'],
 });
